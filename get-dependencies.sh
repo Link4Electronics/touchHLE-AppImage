213ac90e52b057261f70e20d0a45f9ccae18aaa0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-05 15:23:06 -0400

    build the thing

---
+++ get-dependencies.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -eu
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	android-tools     \
+	base-devel        \
+	curl              \
+	git               \
+	libx11            \
+	libxrandr         \
+	libxss            \
+	pulseaudio        \
+	pulseaudio-alsa   \
+	wget              \
+	xorg-server-xvfb  \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl --prefer-nano
+
+echo "Building touchhle..."
+echo "---------------------------------------------------------------"
+sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i \
+	-e 's|-O2|-O3|'                              \
+	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
+	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
+	/etc/makepkg.conf
+cat /etc/makepkg.conf
+
+git clone --depth 1 https://aur.archlinux.org/touchhle.git ./touchhle
+(
+	cd ./touchhle
+	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
+	makepkg -fs --noconfirm
+	ls -la ./
+	pacman --noconfirm -U ./*.pkg.tar.*
+)
